10d68268d4adffe80a0a3401fe375dbae524dd50
XGQT <xgqt@protonmail.com>
2019-06-23 23:00:00 +0200

    rename ini2 to config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -36,7 +36,21 @@
   (package-install 'use-package))
 
 ;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
-(when (file-readable-p "~/.emacs.d/ini2.org")
-  (org-babel-load-file (expand-file-name "~/.emacs.d/ini2.org")))
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (dashboard org-bullets auto-complete web-mode dired-sidebar sudo-edit magit projectile beacon avy rainbow-delimiters rainbow-mode yasnippet flycheck diminish spaceline spacemacs-theme switch-window smex ido-vertical-mode which-key auto-package-update use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
