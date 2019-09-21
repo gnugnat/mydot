28840df3eb8643a808ad53178480288ae70d0188
XGQT <4181308-xgqt@users.noreply.gitlab.com>
2019-09-22 01:08:11 +0200

    emacs: move forward :)

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,8 +1,12 @@
-;;; init.el
-;;; delete your ~/.emacs file
-;;; place this file in ~/.emacs.d (as ~/.emacs.d/init.el)
+;;; init --- initialization
+;;; Commentary:
+;;	delete ~/.emacs file
+;;	place this file in
+;;	~/.emacs.d directory
 
-;;; Fixes garbage collection, makes Emacs start up faster
+;;; Code:
+
+;; Fixes garbage collection, makes Emacs start up faster
 (setq gc-cons-threshold 402653184
       gc-cons-percentage 0.6)
 
@@ -19,38 +23,27 @@
 (add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
 (add-hook 'emacs-startup-hook 'startup/reset-gc)
 
-;;; package archives
+;; package archives
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives '(
 			 ("ELPA"  . "http://tromey.com/elpa/")
 			 ("gnu"   . "http://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")
-			 ("org"   . "https://orgmode.org/elpa/")
+			 ("ORG"   . "https://orgmode.org/elpa/")
 			 ))
 (package-initialize)
 
-;;; install use-package
+;; install use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
+;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
+;; Set path to store "custom-set"
+(setq custom-file "~/.emacs.d/emacs-custom.el")
+
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (dashboard org-bullets auto-complete web-mode dired-sidebar sudo-edit magit projectile beacon avy rainbow-delimiters rainbow-mode yasnippet flycheck diminish spaceline spacemacs-theme switch-window smex ido-vertical-mode which-key auto-package-update use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
