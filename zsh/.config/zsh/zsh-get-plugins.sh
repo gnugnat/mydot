06e78fa057ee7facb1a5b6adb158f206208f8044
XGQT <xgqt@protonmail.com>
2020-03-13 15:59:47 +0100

    zsh: update plugin downloader

--- zsh/.config/zsh/zsh-get-plugins.sh
+++ zsh/.config/zsh/zsh-get-plugins.sh
@@ -1,13 +1,63 @@
 #!/usr/bin/env zsh
 
+
 # Get the required zsh plugins
+
 
 # set ZDOTDIR if it is not set
 ZDOTDIR=${ZDOTDIR:-$HOME/.config/zsh}
 
-# make plugins directory
-mkdir "$ZDOTDIR/plugins"
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZDOTDIR/plugins/zsh-syntax-highlighting
+ask_for_download() {
 
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZDOTDIR/plugins/zsh-autosuggestions
+    # input: "user/repo" on Github
+    plugin_name="$(basename ${1})"
+
+    echo
+    tput bold
+    echo "Download ${plugin_name}? [Y/n]"
+    tput sgr0
+
+    read -r dow_ans
+
+    case ${dow_ans} in
+        [nN][oO]|[nN])
+            echo "Not downloading ${plugin_name}"
+            ;;
+        *)
+            plugin_home="$ZDOTDIR/plugins/${plugin_name}"
+            if [ -d ${plugin_home} ]
+            then
+                cd ${plugin_home}
+                git pull -v
+            else
+                git clone -v https://github.com/${1} ${plugin_home}
+            fi
+            ;;
+    esac
+
+    unset plugin_name
+    unset dow_ans
+}
+
+
+# Check if git exists
+if ! command -v git >/dev/null
+then
+    echo "No git found, exiting"
+    exit 1
+fi
+
+# Make the plugins directory
+mkdir -p "$ZDOTDIR/plugins"
+
+# Plugins:
+ask_for_download zsh-users/zsh-syntax-highlighting
+ask_for_download zsh-users/zsh-autosuggestions
+
+# Finish
+echo
+tput bold
+echo "Finished downloading plugins"
+tput sgr0
+echo
