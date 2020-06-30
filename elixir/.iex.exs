48d9333884cdaf5e113acbf94fbe7425c87b096d
Maciej Barć <xgqt@protonmail.com>
2020-06-30 03:45:28 +0200

    elixir: add iex config

---
+++ elixir/.iex.exs
@@ -0,0 +1,59 @@
+# Based upon:
+#  https://github.com/blackode/custom-iex
+
+
+# Additional environment settings
+
+# Import ecto quries for testing quries in iex
+import_if_available(Ecto.Query)
+
+# Enable ANSI
+Application.put_env(:elixir, :ansi_enabled, true)
+
+# Color
+magenta_ansi = IO.ANSI.magenta()
+reset_ansi = IO.ANSI.reset()
+
+
+# Prompt
+
+IEx.configure(
+  colors: [enabled: true],
+  # This will display when we enter multiple lines of code
+  continuation_prompt: magenta_ansi <> "... " <> reset_ansi,
+  default_prompt:
+  [
+    # ANSI CHA, move cursor to column 1
+    "\e[G",
+    :yellow,
+    #"%prefix",
+    #"(%counter)",
+    :magenta,
+    # UTF "❯" symbol
+    "\u276f\u276f\u276f",
+    :reset
+  ]
+  |> IO.ANSI.format()
+  |> IO.chardata_to_string()
+)
+
+
+# Greeter
+
+greeting_text = magenta_ansi <>
+  "\n" <>
+  "        .:        \n" <>
+  "      .::.        \n" <>
+  "    .lllc:.       \n" <>
+  "   .xo::::l.      \n" <>
+  "  .O'.''.looc.    \n" <>
+  "  .c..::.oxx.:.   \n" <>
+  " ..'...'.dkkc..'  \n" <>
+  " '''....lOOOd'''. \n" <>
+  " ......k0000k.... \n" <>
+  "  coloKXXXXKc:cc. \n" <>
+  "   'lkNNNNKd...   \n" <>
+  "     ':xxll:'     \n" <>
+  reset_ansi
+
+IO.puts(greeting_text)
