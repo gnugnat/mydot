0c6d55e2347336004864a1eacf1975b65c50c9c6
Maciej Barć <xgqt@protonmail.com>
2020-07-23 18:35:34 +0200

    emacs: reformat init

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,49 +1,65 @@
 ;;; init --- initialization
 ;;; Commentary:
-;;	delete ~/.emacs file
-;;	place this file in
-;;	~/.emacs.d directory
+;;  delete ~/.emacs file
+;;  place this file in
+;;  ~/.emacs.d directory
 
 ;;; Code:
 
-;; Fixes garbage collection, makes Emacs start up faster
-(setq gc-cons-threshold 402653184
-      gc-cons-percentage 0.6)
+
+;; Fix garbage collection (makes Emacs start up faster)
+
+(setq
+ gc-cons-threshold 402653184
+ gc-cons-percentage 0.6
+ )
 
 (defvar startup/file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 
 (defun startup/revert-file-name-handler-alist ()
-  (setq file-name-handler-alist startup/file-name-handler-alist))
+  (setq file-name-handler-alist startup/file-name-handler-alist)
+  )
 
 (defun startup/reset-gc ()
-  (setq gc-cons-threshold 16777216
-	gc-cons-percentage 0.1))
+  (setq
+   gc-cons-threshold 16777216
+   gc-cons-percentage 0.1
+   )
+  )
 
 (add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
 (add-hook 'emacs-startup-hook 'startup/reset-gc)
 
+
+;; Packages
+
 ;; package archives
 (require 'package)
 (setq package-enable-at-startup nil)
-(setq package-archives '(
-			 ("ELPA"  . "http://tromey.com/elpa/")
-			 ("gnu"   . "http://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")
-			 ("ORG"   . "https://orgmode.org/elpa/")
-			 ))
+(setq package-archives
+      '(
+        ("ELPA"  . "http://tromey.com/elpa/")
+        ("gnu"   . "http://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")
+        ("ORG"   . "https://orgmode.org/elpa/")
+        )
+      )
 (package-initialize)
 
 ;; install use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
-  (package-install 'use-package))
+  (package-install 'use-package)
+  )
+
+
+;; Load other custom components
 
 ;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
 (when (file-readable-p "~/.emacs.d/config.org")
-  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
   )
-)
 
 ;; Set path to store "custom-set"
 (setq custom-file "~/.emacs.d/emacs-custom.el")
