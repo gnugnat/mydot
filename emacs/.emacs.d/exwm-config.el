27ce3e085ddf10409c59d501c10464b74a8d994d
Maciej Barć <xgqt@protonmail.com>
2021-02-06 21:34:37 +0100

    emacs/exwm-config.el: format
    Signed-off-by: Maciej Barć <xgqt@protonmail.com>

--- emacs/.emacs.d/exwm-config.el
+++ emacs/.emacs.d/exwm-config.el
@@ -46,8 +46,10 @@
   (fringe-mode 10)
 
   ;; create clock
-  (setq display-time-24hr-format t
-	display-time-format "%H:%M %d %b")
+  (setq
+   display-time-24hr-format t
+   display-time-format "%H:%M %d %b"
+   )
   (display-time-mode 1)
 
   ;; make sure dashboard shows up
@@ -59,21 +61,27 @@
   ;; only enable the connected external screen
   (require 'exwm-randr)
   (defun exwm-change-screen-hook ()
-    (let ((xrandr-output-regexp "\n\\([^ ]+\\) connected ")
-	  default-output)
+    (let
+        (
+         (xrandr-output-regexp "\n\\([^ ]+\\) connected ")
+         default-output
+         )
       (with-temp-buffer
-	(call-process "xrandr" nil t nil)
-	(goto-char (point-min))
-	(re-search-forward xrandr-output-regexp nil 'noerror)
-	(setq default-output (match-string 1))
-	(forward-line)
-	(if (not (re-search-forward xrandr-output-regexp nil 'noerror))
-	    (call-process "xrandr" nil nil nil "--output" default-output "--auto")
-	  (call-process
-	   "xrandr" nil nil nil
-	   "--output" (match-string 1) "--primary" "--auto"
-	   "--output" default-output "--off")
-	  (setq exwm-randr-workspace-output-plist (list 0 (match-string 1))))))
+        (call-process "xrandr" nil t nil)
+        (goto-char (point-min))
+        (re-search-forward xrandr-output-regexp nil 'noerror)
+        (setq default-output (match-string 1))
+        (forward-line)
+        (if (not (re-search-forward xrandr-output-regexp nil 'noerror))
+            (call-process "xrandr" nil nil nil "--output" default-output "--auto")
+          (call-process
+           "xrandr" nil nil nil
+           "--output" (match-string 1) "--primary" "--auto"
+           "--output" default-output "--off")
+          (setq exwm-randr-workspace-output-plist (list 0 (match-string 1)))
+          )
+        )
+      )
     ;; set the wallpaper (with feh)
     (start-process-shell-command
      "feh" nil "sh -c '[ -f $HOME/.fehbg ] && $HOME/.fehbg'")
@@ -83,7 +91,8 @@
 
   ;; rename buffers to window title
   (defun exwm-rename-buffer-to-title ()
-    (exwm-workspace-rename-buffer exwm-title))
+    (exwm-workspace-rename-buffer exwm-title)
+    )
   (add-hook 'exwm-update-title-hook 'exwm-rename-buffer-to-title)
 
   ;; systray
@@ -91,16 +100,21 @@
   (exwm-systemtray-enable)
 
   ;; workspaces
-  (setq exwm-workspace-number 10
-	exwm-workspace-show-all-buffers nil
-	exwm-layout-show-all-buffers nil)
+  (setq
+   exwm-workspace-number 10
+   exwm-workspace-show-all-buffers nil
+   exwm-layout-show-all-buffers nil
+   )
 
   ;; bind s-<number> to switch to the corresponding workspace
   (dotimes (i 10)
-    (exwm-input-set-key (kbd (format "s-%d" i))
-			`(lambda ()
-			   (interactive)
-			   (exwm-workspace-switch-create ,i))))
+    (exwm-input-set-key
+     (kbd (format "s-%d" i))
+     `(lambda ()
+        (interactive)
+        (exwm-workspace-switch-create ,i))
+     )
+    )
 
   ;; rebind essential GUI keys
   (exwm-input-set-simulation-keys
@@ -121,99 +135,135 @@
      ([?\C-y] . ?\C-v)
      ;; search
      ([?\C-s] . ?\C-f)
-     ))
+     )
+   )
 
   (setq exwm-input-global-keys
-	'(
-
-	  ;; exit char-mode and fullscreen mode
-	  ([?\s-r] . exwm-reset)
-
-	  ;; switch workspace interactively
-	  ([?\s-w] . exwm-workspace-switch)
-
-	  ;; "s-<f2>" to lock screen
-	  ([s-f2] . (lambda ()
-		      (interactive)
-		      (start-process-shell-command
-		       "xset" nil "xset s activate")))
-	  ))
+        '(
+          ;; exit char-mode and fullscreen mode
+          ([?\s-r] . exwm-reset)
+          ;; switch workspace interactively
+          ([?\s-w] . exwm-workspace-switch)
+          ;; "s-<f2>" to lock screen
+          ([s-f2] . (lambda ()
+                      (interactive)
+                      (start-process-shell-command
+                       "xset" nil "xset s activate")
+                      )
+           )
+          )
+        )
 
   ;; Switch to last open buffer
   (defun switch-to-last-buffer()
     "Switch to last open buffer in current window"
     (interactive)
-    (switch-to-buffer (other-buffer (current-buffer) 1)))
+    (switch-to-buffer (other-buffer (current-buffer) 1))
+    )
   (exwm-input-set-key (kbd "s-<tab>") #'switch-to-last-buffer)
 
   ;;; Fn keys
-  ;;
   ;; audio
-  (exwm-input-set-key (kbd "<XF86AudioRaiseVolume>")
-		      (lambda ()
-			(interactive)
-			(start-process-shell-command
-			 "pactl" nil "pactl set-sink-volume $(pactl list short sinks | grep RUNNING | cut -f1) +2%")))
-  (exwm-input-set-key (kbd "<XF86AudioLowerVolume>")
-		      (lambda ()
-			(interactive)
-			(start-process-shell-command
-			 "pactl" nil "pactl set-sink-volume $(pactl list short sinks | grep RUNNING | cut -f1) -2%")))
-  (exwm-input-set-key (kbd "<XF86AudioMute>")
-		      (lambda ()
-			(interactive)
-			(start-process-shell-command
-			 "pactl" nil "pactl set-sink-mute $(pactl list short sinks | grep RUNNING | cut -f1) toggle")))
-  ;;
+  (exwm-input-set-key
+   (kbd "<XF86AudioRaiseVolume>")
+   (lambda ()
+     (interactive)
+     (start-process-shell-command
+      "pactl" nil "pactl set-sink-volume $(pactl list short sinks | grep RUNNING | cut -f1) +2%"
+      )
+     )
+   )
+  (exwm-input-set-key
+   (kbd "<XF86AudioLowerVolume>")
+   (lambda ()
+     (interactive)
+     (start-process-shell-command
+      "pactl" nil "pactl set-sink-volume $(pactl list short sinks | grep RUNNING | cut -f1) -2%"
+      )
+     )
+   )
+  (exwm-input-set-key
+   (kbd "<XF86AudioMute>")
+   (lambda ()
+     (interactive)
+     (start-process-shell-command
+      "pactl" nil "pactl set-sink-mute $(pactl list short sinks | grep RUNNING | cut -f1) toggle"
+      )
+     )
+   )
   ;; mpd
-  (exwm-input-set-key (kbd "<XF86AudioPrev>")
-		      (lambda ()
-			(interactive)
-			(start-process-shell-command
-			 "mpc" nil "mpc prev")))
-  (exwm-input-set-key (kbd "<XF86AudioNext>")
-		      (lambda ()
-			(interactive)
-			(start-process-shell-command
-			 "mpc" nil "mpc next")))
-  (exwm-input-set-key (kbd "<XF86AudioPlay>")
-		      (lambda ()
-			(interactive)
-			(start-process-shell-command
-			 "mpc" nil "mpc toggle")))
-  (exwm-input-set-key (kbd "<XF86AudioStop>")
-		      (lambda ()
-			(interactive)
-			(start-process-shell-command
-			 "mpc" nil "mpc stop")))
-  ;;
+  (exwm-input-set-key
+   (kbd "<XF86AudioPrev>")
+   (lambda ()
+     (interactive)
+     (start-process-shell-command
+      "mpc" nil "mpc prev"
+      )
+     )
+   )
+  (exwm-input-set-key
+   (kbd "<XF86AudioNext>")
+   (lambda ()
+     (interactive)
+     (start-process-shell-command
+      "mpc" nil "mpc next"
+      )
+     )
+   )
+  (exwm-input-set-key
+   (kbd "<XF86AudioPlay>")
+   (lambda ()
+     (interactive)
+     (start-process-shell-command
+      "mpc" nil "mpc toggle"
+      )
+     )
+   )
+  (exwm-input-set-key
+   (kbd "<XF86AudioStop>")
+   (lambda ()
+     (interactive)
+     (start-process-shell-command
+      "mpc" nil "mpc stop"
+      )
+     )
+   )
   ;; camera
-  (exwm-input-set-key (kbd "<XF86Display>")
-		      (lambda ()
-			(interactive)
-			(start-process-shell-command
-			 "mpv" nil "mpv /dev/video0")))
-
+  (exwm-input-set-key
+   (kbd "<XF86Display>")
+   (lambda ()
+     (interactive)
+     (start-process-shell-command
+      "mpv" nil "mpv /dev/video0"
+      )
+     )
+   )
   ;; simple launcher
-  (exwm-input-set-key (kbd "<s-return>")
-		      (lambda (command)
-			(interactive (list (read-shell-command "» ")))
-			(start-process-shell-command command nil command)))
+  (exwm-input-set-key
+   (kbd "<s-return>")
+   (lambda (command)
+     (interactive (list (read-shell-command "» ")))
+     (start-process-shell-command command nil command)
+     )
+   )
 
-  ;; start some programs
-  ;;	if this file is re-loaded those programs will not start
-  ;;	because they are added to emacs-startup-hook
-  (add-hook 'emacs-startup-hook (lambda ()
-				  (start-process-shell-command "" nil "compton -b &")
-				  (start-process-shell-command "" nil "dunst &")
-				  (start-process-shell-command "" nil "/usr/libexec/polkit-gnome-authentication-agent-1 &")
-				  (start-process-shell-command "" nil "mpd &")
-				  (start-process-shell-command "" nil "udiskie -at &")
-				  (start-process-shell-command "" nil "nm-applet &")
-				  (start-process-shell-command "" nil "pasystray &")
-				  (start-process-shell-command "" nil "xfce4-power-manager &")
-				  (start-process-shell-command "" nil "xss-lock -- i3lock -c 000000 &")
-				  ))
+  ;;; start some programs
+  ;; if this file is re-loaded those programs will not start
+  ;; because they are added to emacs-startup-hook
+  (add-hook
+   'emacs-startup-hook
+   (lambda ()
+     (start-process-shell-command "" nil "compton -b &")
+     (start-process-shell-command "" nil "dunst &")
+     (start-process-shell-command "" nil "/usr/libexec/polkit-gnome-authentication-agent-1 &")
+     (start-process-shell-command "" nil "mpd &")
+     (start-process-shell-command "" nil "udiskie -at &")
+     (start-process-shell-command "" nil "nm-applet &")
+     (start-process-shell-command "" nil "pasystray &")
+     (start-process-shell-command "" nil "xfce4-power-manager &")
+     (start-process-shell-command "" nil "xss-lock -- i3lock -c 000000 &")
+     )
+   )
 
   ;; enable
   (exwm-enable)
