b2b48577592e47457cb10d88034ee2a6aa71e42a
Maciej Barć <xgqt@riseup.net>
2021-03-30 10:53:21 +0200

    scripts/emacs_reload_config.sh: add new
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

---
+++ scripts/emacs_reload_config.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+
+# This file is part of mydot.
+
+# mydot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+
+# mydot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with mydot.  If not, see <https://www.gnu.org/licenses/>.
+
+# Copyright (c) 2021, Maciej Barć <xgqt@riseup.net>
+# Licensed under the GNU GPL v3 License
+
+
+trap 'exit 128' INT
+export PATH
+set -e
+
+
+cd "${USER_EMACS_DIRECTORY:-${HOME}/.config/emacs}"
+
+rm ./config.el
+sh ./load.sh --batch
