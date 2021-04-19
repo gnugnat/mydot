9526a3ec2c89fffd39eba5bc18550c9cb0adfc78
Maciej Barć <xgqt@riseup.net>
2021-04-19 10:34:28 +0200

    src/sh: add 70-guix.sh
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

---
+++ src/sh/.config/sh/70-guix.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+
+# This file is part of mydot.
+
+# mydot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+
+# mydot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with mydot.  If not, see <https://www.gnu.org/licenses/>.
+
+# Copyright (c) 2020-2021, Maciej Barć <xgqt@riseup.net>
+# Licensed under the GNU GPL v3 License
+
+# For GNU Guix
+
+
+_guix_profile="${HOME}/.guix-profile"
+
+if [ -e "${_guix_profile}" ]
+then
+   GUIX_PROFILE="${_guix_profile}"
+   source_file "${GUIX_PROFILE}/etc/profile"
+   GUIX_LOCPATH="${_guix_profile}/lib/locale"
+   export GUIX_LOCPATH
+fi
+
+unset _guix_profile
