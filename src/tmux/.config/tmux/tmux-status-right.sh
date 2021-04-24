44e87aeeeeb9cc2d836f24978f0a4c920ff2acf0
Maciej Barć <xgqt@riseup.net>
2021-04-25 01:54:22 +0200

    src/tmux/tmux-status-right.sh: rewrite for 3.2
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

--- src/tmux/.config/tmux/tmux-status-right.sh
+++ src/tmux/.config/tmux/tmux-status-right.sh
@@ -10,30 +10,40 @@
 # shellcheck disable=2016
 
 
-printf "%s" '#(${HOME}/.config/tmux/tmux-battery.sh)'
+printf_script() {
+    printf "%s" "$(exec "${HOME}/.config/tmux/${1}")"
+}
 
-printf "%s" ' '
-printf "%s" '#[bg=#222222,fg=#cccccc]'
-printf "%s" ' '
 
-printf "%s" '#(${HOME}/.config/tmux/tmux-ram.sh)'
+printf_script "tmux-battery.sh"
 
-printf "%s" ' '
-printf "%s" '#[bg=#333333,fg=#bbbbbb]'
-printf "%s" ' '
 
-printf "%s" '#(${HOME}/.config/tmux/tmux-thermal.sh)'
+printf ' '
+printf '#[bg=#222222,fg=#cccccc]'
+printf ' '
 
-printf "%s" ' '
-printf "%s" '#[bg=#444444,fg=#eeeeee]'
-printf "%s" ' '
+printf_script "tmux-ram.sh"
 
-printf "%s" '#(${HOME}/.config/tmux/tmux-load.sh)'
 
-printf "%s" ' '
-printf "%s" '#[bg=#555555,fg=#ffffff]'
-printf "%s" ' '
+printf ' '
+printf '#[bg=#333333,fg=#bbbbbb]'
+printf ' '
 
-printf "%s" '#(date +"%b %d %H:%M")'
+printf_script "tmux-thermal.sh"
 
-printf "%s" ' '
+
+printf ' '
+printf '#[bg=#444444,fg=#eeeeee]'
+printf ' '
+
+printf_script "tmux-load.sh"
+
+
+printf ' '
+printf '#[bg=#555555,fg=#ffffff]'
+printf ' '
+
+printf "%s" "$(date +"%b %d %H:%M")"
+
+
+printf ' '
