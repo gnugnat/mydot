b96f0ef5bfbd506b42d92a8f3368d4bbc3002b45
Maciej Barć <xgqt@riseup.net>
2021-05-23 20:24:56 +0200

    src/emacs: erc - move channels list to erc-channels.el
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

---
+++ src/emacs/.config/emacs/assets/site-lisp/erc-channels.el
@@ -0,0 +1,95 @@
+;;; erc-channels.el --- small tweaks that don't require packages.
+
+
+;; This file is part of mydot.
+
+;; mydot is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, version 3.
+
+;; mydot is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with mydot.  If not, see <https://www.gnu.org/licenses/>.
+
+;; Copyright (c) 2021, Maciej Barć <xgqt@riseup.net>
+;; Licensed under the GNU GPL v3 License
+
+
+
+;;; Commentary:
+
+;; Set a list of IRC channels to automatically join.
+
+;; Use with mydot: https://gitlab.com/xgqt/mydot
+
+
+
+;;; Code:
+
+
+(require 'erc-join)
+
+
+(setq
+ erc-autojoin-channels-alist
+ '(
+   ;; Libera - https://libera.chat/
+   (".*\\.libera.chat"
+    "#blastwave"
+    "#gentoo"
+    "#gentoo-chat"
+    "#gentoo-dev"
+    "#gentoo-guru"
+    "#gentoo-proxy-maint"
+    "#gentoo-qt"
+    "#guix"
+    "#kde"
+    "#lisp"
+    "#racket"
+    "#scheme"
+    "#termux"
+    )
+   ;; Freenode - https://freenode.net/
+   (".*\\.freenode.net"
+    "#NetBSD"
+    "#gentoo"
+    "#gentoo-chat"
+    "#gentoo-dev"
+    "#gentoo-guru"
+    "#gentoo-laptop"
+    "#gentoo-ml"
+    "#gentoo-openbsd"
+    "#gentoo-prefix"
+    "#gentoo-proxy-maint"
+    "#gentoo-qt"
+    "#gentoo-server"
+    "#guix"
+    "#kde"
+    "#lisp"
+    "#techbytes"
+    "#techrights"
+    "#termux"
+    )
+   ;; OFTC - https://www.oftc.net/
+   (".*\\.oftc.net"
+    "#alpine-devel"
+    "#alpine-linux"
+    "#gentoo"
+    )
+   ;; Rizon - https://rizon.net/
+   (".*\\.rizon.net"
+    "#cloveros"
+    )
+   )
+ )
+
+
+(provide 'erc-channels)
+
+
+
+;;; erc-channels.el ends here
