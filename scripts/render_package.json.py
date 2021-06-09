ee4bbbd5d4a58194e4fc8a1de69141bb56d2e94d
Maciej Barć <xgqt@riseup.net>
2021-06-10 01:40:20 +0200

    scripts: add render_package.json.py
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

---
+++ scripts/render_package.json.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+
+"""
+This file is part of mydot.
+
+mydot is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+mydot is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with mydot.  If not, see <https://www.gnu.org/licenses/>.
+
+Copyright (c) 2021, Maciej Barć <xgqt@riseup.net>
+Licensed under the GNU GPL v3 License
+"""
+
+
+import argparse
+
+from jinja2 import Template
+
+
+def render_package_json(version: str):
+    """Render package.json"""
+
+    with open("./package.json.j2", "r") as templete_file:
+        template = Template(templete_file.read()).render(version=version)
+
+    with open("./package.json", "w") as output_file:
+        output_file.write(template)
+
+
+def main():
+    """The main function."""
+
+    parser = argparse.ArgumentParser(
+        description="%(prog)s - generate package.json from package.json.j2",
+        epilog='''Copyright (c) 2021, Maciej Barć <xgqt@riseup.net>
+    Licensed under the GNU GPL v3 License'''
+    )
+    parser.add_argument(
+        "version",
+        type=str,
+        help="version string to use, following 'semver', ie.: 9.9.9"
+    )
+    args = parser.parse_args()
+
+    render_package_json(args.version)
+
+
+if __name__ == '__main__':
+    main()
